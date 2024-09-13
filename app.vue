dde8914769518fd701f371a3cd8259a7e38216e2
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-13 08:10:49 +0200

    :rocket: Add toast component from shadcn

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import { IconoirProvider } from "@iconoir/vue";
+import { Toaster } from "~/components/ui/toast";
 </script>
 
 <template>
   <div>
     <NuxtRouteAnnouncer />
+    <Toaster />
     <IconoirProvider
       :icon-props="{
         'width': '1rem',
