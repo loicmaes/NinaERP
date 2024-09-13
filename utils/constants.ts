f4aeff3a34a9a37b6847eec35bd94d5d6a06b8a1
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-13 17:27:24 +0200

    :rocket: Add password reset request code sending email template

--- utils/constants.ts
+++ utils/constants.ts
@@ -3,3 +3,4 @@ export const userUidCookie = "user-uid";
 
 export const platformUrl = () => useRuntimeConfig().public.platformUrl;
 export const userEmailVerificationLink = (code: string) => `${platformUrl()}/portal/auth/email/verify/${code}`;
+export const userPasswordResetRequestCompletion = (code: string) => `${platformUrl()}/portal/auth/password/reset/${code}`;
