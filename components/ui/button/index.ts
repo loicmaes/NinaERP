8f63cc8c4655d8a928d6fd89bd5133353b62174f
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-09-17 05:31:42 +0200

    :rocket: Add variant to button component

--- components/ui/button/index.ts
+++ components/ui/button/index.ts
@@ -12,6 +12,7 @@ export const buttonVariants = cva(
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outlineDestructive: "border border-destructive text-destructive hover:bg-destructive hover:text-destructive-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
