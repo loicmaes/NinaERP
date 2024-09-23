b1d452bb840e44d4399ffa94a91709af09d37502
loicmaes <production@maesloic.fr>
2024-09-23 10:54:54 +0200

    :rocket: Add user avatar component
    Signed-off-by: loicmaes <production@maesloic.fr>

---
+++ components/layouts/app/appNavigation/UserAvatar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Avatar, AvatarFallback } from "~/components/ui/avatar";
+import type { User } from "~/types/user";
+
+const user = useState<User>("user");
+</script>
+
+<template>
+  <Avatar shape="square">
+    <AvatarFallback>{{ user.userInfo?.firstName.substring(0, 1).toUpperCase() }}{{ user.userInfo?.lastName.substring(0, 1).toUpperCase() }}</AvatarFallback>
+  </Avatar>
+</template>
+
+<style scoped>
+
+</style>
