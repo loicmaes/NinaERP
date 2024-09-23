49c81796208b4f3d26a87b5b53381d76acb63985
loicmaes <production@maesloic.fr>
2024-09-23 16:05:51 +0200

    :rocket: Update user avatar props
    Signed-off-by: loicmaes <production@maesloic.fr>

--- components/layouts/app/appNavigation/UserAvatar.vue
+++ components/layouts/app/appNavigation/UserAvatar.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
-import { Avatar, AvatarFallback } from "~/components/ui/avatar";
+import type { HTMLAttributes } from "vue";
+import { Avatar, AvatarFallback, type AvatarVariants } from "~/components/ui/avatar";
 import type { User } from "~/types/user";
+import { cn } from "~/lib/utils";
 
 const user = useState<User>("user");
+
+const props = withDefaults(defineProps<{
+  shape?: AvatarVariants["shape"];
+  size?: AvatarVariants["size"];
+  class?: HTMLAttributes["class"];
+}>(), {
+  shape: "square",
+  size: "sm",
+});
 </script>
 
 <template>
-  <Avatar shape="square">
+  <Avatar
+    :shape="props.shape"
+    :size="props.size"
+    :class="cn(props.class)"
+  >
     <AvatarFallback>{{ user.userInfo?.firstName.substring(0, 1).toUpperCase() }}{{ user.userInfo?.lastName.substring(0, 1).toUpperCase() }}</AvatarFallback>
   </Avatar>
 </template>
